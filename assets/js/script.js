e37b1fbb92c46f17df4831300c706c3faf1ec755
Edoardo <edfl.tunzi@gmail.com>
2024-10-17 15:11:16 +0200

    JS generate board func, number extraction func and button created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,34 @@
-const generateBoard = function () {};
+// funzione che genera il tabellone
+const generateBoard = function () {
+  const board = document.getElementById("board");
+
+  for (let i = 1; i < 91; i++) {
+    let numCell = document.createElement("h3");
+    numCell.classList.add("cell");
+    numCell.innerText = i;
+
+    board.appendChild(numCell);
+  }
+};
+//array per tenere traccia dei numeri estratti
+const extractedNumbers = [];
+//funzione che genera numero random 1 a 90
+const generateRandomNum = function () {
+  const randomNum = document.getElementById("randomNum");
+  let randomGenNum = Math.floor(Math.random() * 90) + 1;
+
+  while (!extractedNumbers.includes(randomGenNum)) {
+    randomNum.innerText = randomGenNum;
+    extractedNumbers.push(randomGenNum);
+    break;
+  }
+  console.log(extractedNumbers);
+};
+
+const randomBtn = document.getElementById("randomBtn");
+randomBtn.onclick = function () {
+  generateRandomNum();
+};
 
 window.onload = () => {
   generateBoard();
