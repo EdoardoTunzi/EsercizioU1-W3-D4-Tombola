cfc3d0af9f305f7f3ad47847d923a8d9dd5baac1
Edoardo <edfl.tunzi@gmail.com>
2024-10-17 16:44:51 +0200

    Player board function created. player number selection logic needs to be fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,8 @@ const generateBoard = function () {
 const generateRandomNum = function () {
   const randomNum = document.getElementById("randomNum");
   let randomGenNum = Math.floor(Math.random() * 90) + 1;
-
+  /*controllo se il numero generato non è già stato estratto, 
+se non lo è, lo mostro e lo aggiungo a extractedNumbers*/
   if (!extractedNumbers.includes(randomGenNum)) {
     randomNum.innerText = randomGenNum;
     extractedNumbers.push(randomGenNum);
@@ -34,6 +35,33 @@ randomBtn.onclick = function () {
   generateRandomNum();
 };
 
+const playerNumbers = [];
+//funzione per generare tabella Player
+const generatePlayerBoard = function () {
+  const playerBoard = document.getElementById("playerBoard");
+  //playerBoard.innerHTML = ""; //pulisco l'html perche rigenero la funzione quando genero un numero col button
+  for (let i = 1; i < 25; i++) {
+    let numCell = document.createElement("h3");
+    numCell.classList.add("cell");
+
+    let randomGenNum = Math.floor(Math.random() * 90) + 1;
+
+    //creo un while per controllare che il numero generato sia presente nell'array di numeri estratti e nel caso gli faccio cambiare sfondo
+    while (playerNumbers.includes(randomGenNum)) {
+      randomGenNum = Math.floor(Math.random() * 90) + 1;
+    }
+    //il numero che esce dal ciclo è unico e lo assegno acell e nell'array playerNUmbers
+    numCell.innerText = randomGenNum;
+    playerNumbers.push(randomGenNum);
+    //controllo se è presente in extractedNumbers
+    if (extractedNumbers.includes(randomGenNum) && playerNumbers.includes(randomGenNum)) {
+      numCell.style.backgroundColor = "coral";
+    }
+    playerBoard.appendChild(numCell);
+  }
+};
+
 window.onload = () => {
   generateBoard();
+  generatePlayerBoard();
 };
