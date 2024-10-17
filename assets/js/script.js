c3b64d671963c2c6f9cd2dfe43fd58e2f347686b
Edoardo <edfl.tunzi@gmail.com>
2024-10-17 15:42:36 +0200

    added if statment to keep extracted number selected on the board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,30 @@
+//array per tenere traccia dei numeri estratti
+const extractedNumbers = [];
 // funzione che genera il tabellone
 const generateBoard = function () {
   const board = document.getElementById("board");
-
+  board.innerHTML = ""; //pulisco l'html perche rigenero la funzione quando genero un numero col button
   for (let i = 1; i < 91; i++) {
     let numCell = document.createElement("h3");
     numCell.classList.add("cell");
     numCell.innerText = i;
-
+    //creo un if per controllare che il numero generato sia presente nell'array di numeri estratti e nel caso gli faccio cambiare sfondo
+    if (extractedNumbers.includes(i)) {
+      numCell.style.backgroundColor = "coral";
+    }
     board.appendChild(numCell);
   }
 };
-//array per tenere traccia dei numeri estratti
-const extractedNumbers = [];
+
 //funzione che genera numero random 1 a 90
 const generateRandomNum = function () {
   const randomNum = document.getElementById("randomNum");
   let randomGenNum = Math.floor(Math.random() * 90) + 1;
 
-  while (!extractedNumbers.includes(randomGenNum)) {
+  if (!extractedNumbers.includes(randomGenNum)) {
     randomNum.innerText = randomGenNum;
     extractedNumbers.push(randomGenNum);
-    break;
+    generateBoard(); // rigenero la board con il numero evidenziato grazie al controllo if in generateBoard()
   }
   console.log(extractedNumbers);
 };
